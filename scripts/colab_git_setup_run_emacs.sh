00202c5a2c5a840cc867a9a3799e6aa296614623
William <yaxingl@usc.edu>
2024-09-24 18:50:05 -0700

    Choose "Yes" by default when installing packages for emacs

--- scripts/colab_git_setup_run_emacs.sh
+++ scripts/colab_git_setup_run_emacs.sh
@@ -4,6 +4,10 @@ BLUE='\033[38;5;33m'
 RESET='\033[0m'
 
 echo -e "${BLUE}Running Emacs in the background to install packages...${RESET}"
-emacs --batch -l ~/.emacs.d/init.el --eval="(progn (package-refresh-contents) (package-install-selected-packages))"
+emacs --batch -l ~/.emacs.d/init.el --eval="(progn
+  (setq package-check-signature nil)
+  (setq package-selected-packages '(smooth-scrolling))
+  (package-refresh-contents)
+  (package-install-selected-packages))"
 
 echo -e "${BLUE}Emacs setup and package installation complete!${RESET}"
