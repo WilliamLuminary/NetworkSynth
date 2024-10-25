705feb9f55218363ab4d1e54239ca382ab011763
William <yaxingl@usc.edu>
2024-10-24 21:48:57 -0700

    Restructure the code

--- scripts/google_colab_setup/setup_git.sh
+++ scripts/google_colab_setup/setup_git.sh
@@ -4,6 +4,10 @@ BLUE='\033[38;5;33m'
 RESET='\033[0m'
 
 echo -e "${BLUE}Setting up Git...${RESET}"
+
+git config --global core.editor "emacs"
+git config --global core.fileMode false
+
 git config --global user.name "Yaxing Li"
 git config --global user.email "93109493+WilliamLuminary@users.noreply.github.com"
 
