0a909e9dccf62467121e1c4470335b5ea855b86c
William <yaxingl@usc.edu>
2024-11-19 01:02:31 -0800

    Set up for local git config

--- scripts/google_colab_setup/setup_git.sh
+++ scripts/google_colab_setup/setup_git.sh
@@ -3,12 +3,14 @@
 BLUE='\033[38;5;33m'
 RESET='\033[0m'
 
-echo -e "${BLUE}Setting up Git...${RESET}"
-
-git config --global core.editor "emacs"
-git config --global core.fileMode false
-
+echo -e "${BLUE}Setting up global Git configurations...${RESET}"
 git config --global user.name "Yaxing Li"
 git config --global user.email "93109493+WilliamLuminary@users.noreply.github.com"
+git config --global core.editor "emacs"
+git config --global core.fileMode false
+echo -e "${BLUE}Setting up local Git configurations...${RESET}"
 
-echo -e "${BLUE}Git configuration complete!${RESET}"
+git config user.name "Yaxing Li (Google Colab)"
+git config user.email "93109493+WilliamLuminary@users.noreply.github.com"
+git config core.fileMode false
+echo -e "${BLUE}Git configuration (global and local) complete!${RESET}"
