ef5711df76e54f989bb94c70a6ad0ec3b7085346
William <yaxingl@usc.edu>
2024-11-19 00:58:57 -0800

    Modify bash codes so we only install emacs by default

--- scripts/google_colab_setup/setup_install.sh
+++ scripts/google_colab_setup/setup_install.sh
@@ -4,6 +4,6 @@ BLUE='\033[38;5;33m'
 RESET='\033[0m'
 
 echo -e "${BLUE}Updating and installing tree and emacs...${RESET}"
-sudo apt update && sudo apt install -y tree emacs
-
+#sudo apt update && sudo apt install -y tree emacs
+sudo apt install -y emacs
 echo -e "${BLUE}Installation complete!${RESET}"
