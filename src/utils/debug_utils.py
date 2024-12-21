810209eb254246bcfeef9eb47efe7f685c99e374
William <93109493+WilliamLuminary@users.noreply.github.com>
2024-12-21 13:37:43 -0800

    - Remove debugging() wrapper

--- src/utils/debug_utils.py
+++ src/utils/debug_utils.py
@@ -5,27 +5,7 @@ import functools
 import time
 from rich import print as rich_print
 
-DEBUG = False  # Set to True to enable debug prints
-
-
-def debugging(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        original_print = builtins.print
-
-        def debug_print(*print_args, debug=True, **print_kwargs):
-            if DEBUG and debug:
-                rich_print(f"[DEBUG] \"{func.__name__}\":", *print_args, **print_kwargs)
-            elif not debug:
-                original_print(*print_args, **print_kwargs)
-
-        builtins.print = debug_print
-        try:
-            return func(*args, **kwargs)
-        finally:
-            builtins.print = original_print
-
-    return wrapper
+DEBUG = False
 
 
 def timer(func):
