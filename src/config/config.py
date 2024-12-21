36989cc8a56a317f43609462a4e03d13e451f75b
William <93109493+WilliamLuminary@users.noreply.github.com>
2024-12-21 02:07:57 -0800

    - Move the base.py to a new path  - Extract constants to the base

--- src/config/config.py
+++ src/config/config.py
@@ -1,32 +1,15 @@
 import os
+import logging
+
+from config.base import BaseConfig
 
 
-class Config:
-    def __init__(self):
-        script_dir = os.path.dirname(os.path.realpath(__file__))
-        project_root = os.path.abspath(os.path.join(script_dir, '..', '..'))
+class Config(BaseConfig):
+    def __init__(self, set_name, resolution):
+        super().__init__()
+        self.set_name = set_name
+        self.resolution = resolution
 
-        self.BASE_DATA_PATH = os.path.join(project_root, 'data')
+        self.SYNTHETIC_GRAPH_PATH = os.path.join(self.OUTPUT_DIR, set_name, self.SYNTHETIC_GRAPH_DIRECTORY_NAME)
+        self.ORIGINAL_GRAPH_PATH = os.path.join(self.OUTPUT_DIR, resolution, self.ORIGINAL_GRAPH_DIRECTORY_NAME)
 
-        self.BASE_INPUT_PATH = os.path.join(self.BASE_DATA_PATH, 'input')
-        self.POSITIONS_DIR = os.path.join(self.BASE_INPUT_PATH, 'position')
-        self.SPARSE_MATRICES_DIR = os.path.join(self.BASE_INPUT_PATH, 'sparse_matrices')
-        self.IMAGES_DIR = os.path.join(self.BASE_INPUT_PATH, 'Original Graphs')
-
-        self.BASE_OUTPUT_PATH = os.path.join(self.BASE_DATA_PATH, 'output')
-        self.OUTPUT_DIR = os.path.join(self.BASE_OUTPUT_PATH, 'Results')
-        self.SYNTHETIC_GRAPH_PATH = os.path.join(self.OUTPUT_DIR, 'Synthetic Graphs')
-        self.ORIGINAL_GRAPH_PATH = os.path.join(self.OUTPUT_DIR, 'Original Graphs')
-
-        self.DEFAULT_FRAME_RANGE = 510
-        self.CLOSED_NODES_FACTOR = 1
-        self.CLOSED_EDGES_FACTOR = 1.5
-
-        self._ensure_directories()
-
-    def _ensure_directories(self):
-        os.makedirs(self.BASE_OUTPUT_PATH, exist_ok=True)
-        os.makedirs(self.OUTPUT_DIR,
-                    exist_ok=True)
-        os.makedirs(self.SYNTHETIC_GRAPH_PATH, exist_ok=True)
-        os.makedirs(self.ORIGINAL_GRAPH_PATH, exist_ok=True)
