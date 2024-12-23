5464265e3ba81b9b8e00476959fbd63fe3609296
William <93109493+WilliamLuminary@users.noreply.github.com>
2024-12-23 03:05:42 -0800

    - Improve log message

--- src/handlers/output_handler.py
+++ src/handlers/output_handler.py
@@ -27,7 +27,7 @@ class OutputHandler(Config):
     def initialize(cls):
         cls.base_output_dir = os.path.join(Config.BASE_OUTPUT_PATH, f'results_{OutputHandler._time_id()}')
         cls.ensure_directory(cls.base_output_dir)
-        print(f"Created new directory: {cls.base_output_dir}")
+        logger.info(f"Created new directory: {cls.base_output_dir}")
 
     @staticmethod
     def archive_if_exists(path: str) -> None:
@@ -37,13 +37,13 @@ class OutputHandler(Config):
                 base, ext = os.path.splitext(path)
                 archived_path = f"{base}_archived_{timestamp}{ext}"
                 os.rename(path, archived_path)
-                logging.info(f"Archived existing file: {archived_path}")
+                logger.info(f"Archived existing file: {archived_path}")
             elif os.path.isdir(path):
                 archived_path = f"{path}_archived_{timestamp}"
                 os.rename(path, archived_path)
-                logging.info(f"Archived existing directory: {archived_path}")
+                logger.info(f"Archived existing directory: {archived_path}")
         else:
-            logging.warning(f"Path does not exist, nothing to archive: {path}")
+            logger.warning(f"Path does not exist, nothing to archive: {path}")
 
     @staticmethod
     def ensure_directory(path: str, exist_ok=False) -> None:
@@ -52,40 +52,39 @@ class OutputHandler(Config):
             flag = True
         os.makedirs(path, exist_ok=exist_ok)
         if flag:
-            logging.info(f"Created new directory: {path}")
+            logger.info(f"Created new directory: {path}")
 
     @staticmethod
     def delete_file(filepath: str) -> None:
         if os.path.exists(filepath) and os.path.isfile(filepath):
             os.remove(filepath)
-            logging.info(f"Deleted existing file: {filepath}")
+            logger.info(f"Deleted existing file: {filepath}")
         elif os.path.isdir(filepath):
-            logging.warning(f"Expected a file but found a directory at: {filepath}")
+            logger.warning(f"Expected a file but found a directory at: {filepath}")
         else:
-            logging.debug(f"No existing file to delete at: {filepath}")
+            logger.debug(f"No existing file to delete at: {filepath}")
 
     def save_file(self, content: Any, data_type: DataType) -> None:
         file_config = Config.FILE_CONFIGURATIONS[data_type]
         rel_path = file_config.relative_dir
-        file_name = f"{self._time_id()}.{file_config.file_type}"
+        file_name = f"{self._time_id()}.{file_config.file_extension}"
         abs_path = os.path.join(self.name_res_output_dir, rel_path, file_name)
-        self.ensure_directory(os.path.dirname(abs_path))
+        self.ensure_directory(os.path.dirname(abs_path), exist_ok=True)
 
         if FileTag.FIG in data_type.tags:
             OutputHandler._save_image(content, abs_path)
-        elif FileTag.PKL in data_type.tags:
+        elif FileTag.DATA in data_type.tags:
             OutputHandler._save_pickle(content, abs_path)
         else:
             raise ValueError(f"Unsupported DataType for saving: {data_type}")
 
-        logging.info(f"Saved file: {abs_path}")
+        logger.info(f"Saved file: {abs_path}")
 
     @staticmethod
     def _save_pickle(obj: Any, filepath: str) -> None:
         with open(filepath, 'wb') as f:
             # noinspection PyTypeChecker
             pickle.dump(obj, f)
-        logging.info(f"Saved pickle file: {filepath}")
 
     @staticmethod
     def _save_image(image: Union[ndarray], filepath: str) -> None:
@@ -93,7 +92,6 @@ class OutputHandler(Config):
             raise ValueError("Unsupported image format. Expected ndarray or plt.Figure.")
 
         cv2.imwrite(filepath, image)
-        logging.info(f"Saved image: {filepath}")
 
     @staticmethod
     def _time_id() -> str:
