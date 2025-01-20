e78c15b9c32c6ac7c7d7a673111ac5d11442922c
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-01-19 18:25:34 -0800

    Refactor image handling and graph transformation logic.
    Updated image loading, resizing, and trimming methods for clarity and efficiency. Simplified node position transformations and modified enum values for consistency. Removed redundant comments and streamlined the code for improved maintainability.

--- src/handlers/data_agent.py
+++ src/handlers/data_agent.py
@@ -2,7 +2,7 @@
 import logging
 import os
 import re
-from typing import Optional
+from typing import Optional, Tuple
 
 import cv2
 import networkx as nx
@@ -38,10 +38,12 @@ class DataAgent(Config):
 
         self._load_image()
         self._resize_image()
+        self._trim_image()
 
         self.original_graph = build_graph_pos_and_adj_mat((self.positions_of_nodes,
                                                            self.adjacency_matrix))
-        self._transform_graph_positions(self.original_graph, rotation_deg=270)
+        self._transform_original_positions(rotation_deg=270)
+        # self._transform_original_positions(rotation_deg=270)
         self.mapper = Mapper(self.original_graph)
         logger.info(f"Data successfully loaded for {self.name_res_set}")
 
@@ -63,9 +65,6 @@ class DataAgent(Config):
         self.attributes = attributes
 
     def _load_positions(self):
-        """
-        Load a single position file matching the new pattern, based on the resolution and set name.
-        """
         directory_path = os.path.join(self.POSITION_DATA_DIR, self.resolution)
         set_name_value = str(self.name_res_set.set_name)  # Convert SetName enum to its string value
         pattern = re.compile(
@@ -85,10 +84,6 @@ class DataAgent(Config):
         return positions
 
     def _load_sparse_matrix(self):
-        """
-        Load the specific sparse matrix from the `sparse_matrices.npz` file, supporting both current files
-        and `SetName` enum types.
-        """
         directory_path = os.path.join(self.ADJ_MATRIX_DATA_DIR, self.resolution)
         file_name = "sparse_matrices.npz"
         file_path = os.path.join(directory_path, file_name)
@@ -96,11 +91,9 @@ class DataAgent(Config):
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"Sparse matrix file not found: {file_path}")
 
-        # Load the .npz archive
         matrix_data = np.load(file_path, allow_pickle=True)
 
-        # Match the key based on the SetName enum value
-        set_name_value = str(self.name_res_set.set_name)  # Convert SetName enum to its string value
+        set_name_value = str(self.name_res_set.set_name)
         key_pattern = rf"W-\d+-\d+-\d+_{re.escape(set_name_value)}_EL"
 
         matching_keys = [key for key in matrix_data.keys() if re.fullmatch(key_pattern, key)]
@@ -159,32 +152,38 @@ class DataAgent(Config):
     #     return matrix_data[set_name].item()
 
     def _load_image(self):
-        images_path = os.path.join(self.IMAGES_DIR, self.set_name)
-        if self.set_name == 'B':
-            # noinspection PyTypeChecker
-            images_path = os.path.join(images_path, '1811')
+        directory_path = os.path.join(self.IMAGES_DIR, self.resolution)
+        set_name_value = str(self.name_res_set.set_name)  # Convert SetName enum to string
+
+        # pattern = re.compile(
+        #     rf"W-\d+-\d+-\d+_{re.escape(set_name_value)}_.*\.(tif|png|jpg)",
+        #     re.IGNORECASE
+        # )
         pattern = re.compile(
-            rf"\b{re.escape(self.resolution)}\b.*\.(tif|png|jpg)",
+            rf"W-\d+-\d+-\d+_{re.escape(set_name_value)}\.(tif|png|jpg)",
             re.IGNORECASE
         )
-        try:
-            file_path = self._find_file_with_pattern(
-                images_path, pattern, f'image for {self.set_name}'
-            )
-        except FileNotFoundError:
-            logger.warning(f"No image found for {self.set_name} at {images_path}. ")
-            self.original_image = None
+
+        file_path = self._find_file_with_pattern(
+            directory_path, pattern, f'image for {self.name_res_set.set_name}'
+        )
+        if file_path is None:
+            logger.warning(f"Background image is None.")
             return
 
         logger.info(f"Image file loaded: {file_path}")
+
         image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
         if image is None:
             err_msg = f"Failed to load image from file: {file_path}"
             logger.warning(err_msg)
+            self.original_image = None
+            return
+
         self.original_image = image
 
     @staticmethod
-    def _find_file_with_pattern(directory_path, pattern, details=''):
+    def _find_file_with_pattern(directory_path, pattern, details='', must_exist=True) -> Optional[str]:
         if not os.path.exists(directory_path):
             raise FileNotFoundError(f"Directory not found: {directory_path}")
 
@@ -197,60 +196,65 @@ class DataAgent(Config):
         elif len(matched_files) > 1:
             raise FileExistsError(f"Multiple {details} files found: {matched_files}.")
         else:
-            raise FileNotFoundError(f"No {details} file found in {directory_path}.")
+            if must_exist:
+                raise FileNotFoundError(f"No {details} file found in {directory_path}.")
+            else:
+                logger.warning(f"No {details} file found in {directory_path}.")
+                return None
 
-    def _resize_image(self, target_size=Config.DEFAULT_FRAME_RANGE):
+    def _resize_image(self, frame_range: Tuple[int, int] = Config.DEFAULT_FRAME_SIZE):
         if self.original_image is None:
             return
-        _image = self.original_image
-        _height, _width = _image.shape
-        _scaling_factor = target_size / max(_width, _height)
+        target_size = min(frame_range)
+        height, width = self.original_image.shape[:2]
+        scaling_factor = target_size / min(height, width)
+        new_width = int(width * scaling_factor)
+        new_height = int(height * scaling_factor)
+        self.original_image = cv2.resize(self.original_image, (new_width, new_height), interpolation=cv2.INTER_LANCZOS4)
 
-        _new_width = int(_width * _scaling_factor)
-        _new_height = int(_height * _scaling_factor)
-        resized_image = cv2.resize(_image, (_new_width, _new_height))
-        self.original_image = resized_image
+    def _trim_image(self, frame_range: Tuple[int, int] = Config.DEFAULT_FRAME_SIZE):
+        if self.original_image is None:
+            return
+        target_size = min(frame_range)
+        height, width = self.original_image.shape[:2]
 
-    @staticmethod
-    def _transform_positions(positions, flip_x=False, flip_y=False, rotation_deg=0):
-        c = Config.DEFAULT_FRAME_RANGE / 2
-        positions -= c
-        if flip_x:
-            positions[:, 0] = -positions[:, 0]
-        if flip_y:
-            positions[:, 1] = -positions[:, 1]
-        if rotation_deg == 90:
-            x = -positions[:, 1].copy()
-            positions[:, 1] = positions[:, 0]
-            positions[:, 0] = x
-        elif rotation_deg == 180:
-            positions = -positions
-        elif rotation_deg == 270:
-            x = positions[:, 1].copy()
-            positions[:, 1] = -positions[:, 0]
-            positions[:, 0] = x
-        positions += c
-        return positions
+        bottom = min(height, target_size)
+        right = min(width, target_size)
+        self.original_image = self.original_image[:bottom, :right]
 
-    @staticmethod
-    def _transform_graph_positions(graph, flip_x=False, flip_y=False, rotation_deg=0):
-        c = Config.DEFAULT_FRAME_RANGE / 2
-        for node, d in graph.nodes(data=True):
+    def _transform_original_positions(self, flip_x=False, flip_y=False, rotation_deg=0):
+        if self.original_image is None:
+            return
+        _graph = self.original_graph
+        __size = self.original_image.shape
+        c = (__size[0] / 2, __size[1] / 2)
+
+        # if rotation_deg != 0 and c[0] != c[1]:
+        #     logger.warning("Only square frames can be rotated. Abort!")
+        #     return
+
+        if not flip_x and not flip_y and rotation_deg == 0:
+            return
+        for node, d in _graph.nodes(data=True):
             p = np.array(d.get('pos', [0, 0]), dtype=np.float64)
-            p -= c
+
+            p[0] -= c[0]
+            p[1] -= c[1]
+
             if flip_x:
                 p[0] = -p[0]
             if flip_y:
                 p[1] = -p[1]
+
             if rotation_deg == 90:
-                px = -p[1]
-                py = p[0]
+                px, py = -p[1], p[0]
                 p[0], p[1] = px, py
             elif rotation_deg == 180:
                 p = -p
             elif rotation_deg == 270:
-                px = p[1]
-                py = -p[0]
+                px, py = p[1], -p[0]
                 p[0], p[1] = px, py
-            p += c
+
+            p[0] += c[0]
+            p[1] += c[1]
             d['pos'] = p
