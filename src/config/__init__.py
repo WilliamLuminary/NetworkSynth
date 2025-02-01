88922dc2df6c39b244801736b1569fffd441ac10
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-02-01 15:41:29 -0800

    Add ConfigSample import in config module

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -3,5 +3,6 @@
 from .config import Config
 from .config_1 import Config1
 from .config_2 import Config2
+from .config_sample import ConfigSample
 from .file_definitions import FileConfig, ImageConfig, PlotConfig, FILE_CONFIGURATIONS
 from .enums import DataType, FileTag, Resolution, SetName
