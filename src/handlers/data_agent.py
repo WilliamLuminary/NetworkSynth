c2eaf07df72f2867da28152ad14c612a8cc58d7e
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-02-13 22:24:47 -0800

    Move function plot_network

--- src/handlers/data_agent.py
+++ src/handlers/data_agent.py
@@ -7,14 +7,17 @@ from typing import List, Optional, Tuple, Union
 import cv2
 import networkx as nx
 import numpy as np
+from matplotlib import pyplot as plt
+from numpy import ndarray
 
 from analysis.multifractal_batch_processor import MultifractalBatchProcessor
-from config import Config, DataType, Resolution, SetName
+from config import Config, DataType, FILE_CONFIGURATIONS, FileTag, Resolution, SetName
+from config.enums import Mode
 from graph import GraphAttrAgent
 from utils import build_graph_pos_and_adj_mat, plot_network
+from utils import build_graph_pos_and_adj_mat, calculate_frame
 from .mapper import Mapper
 from .saver import Saver
-from config.enums import Mode
 
 logger = logging.getLogger(__name__)
 
@@ -222,5 +225,77 @@ class DataAgent:
             self.saver.save_file(self.batch_processor.original, data_type, file_name_prefix)
             self.saver.save_file(self.batch_processor.synthetic, data_type, file_name_prefix)
         elif data_type == DataType.ANALYSIS_FIGURE:
-            for (image_name, image) in self.batch_processor.images:
-                self.saver.save_file(image, data_type, image_name)
+            for image_name, image in self.batch_processor.images.items():
+                self.saver.save_file(image, data_type, f"{image_name}_")
+
+
+def plot_network(data_type: DataType,
+                 graph: nx.Graph,
+                 adjust_axis: bool = False, **kwargs) -> ndarray:
+    """
+    :param data_type:
+    :param graph: If provided, plot the graph directly.
+    :param pos_and_adj_mat: Only used if `graph` is not provided.
+    Tuple / List / Ndarray of positions and adjacency matrix.
+    :param adjust_axis:
+    :param kwargs: Title, frame, plot_in_frame, background, image, alpha, edge_width, node_size, output_path
+    """
+    if not data_type.has_tag(FileTag.PLOT):
+        raise ValueError(f"Invalid data type: {data_type}, only graphs can be passed to this method.")
+
+    file_config = FILE_CONFIGURATIONS.get(data_type)
+    fig, ax = plt.subplots(figsize=(10, 10), dpi=300)
+
+    position_dict = nx.get_node_attributes(graph, 'pos')
+    if adjust_axis and position_dict is not None:
+        positions_array = np.array([position_dict[node] for node in graph.nodes()])
+        positions_array[:, [1, 0]] = positions_array[:, [0, 1]]
+        positions_array[:, 1] = Config.DEFAULT_FRAME_SIZE - positions_array[:, 1]
+        position_dict = {node: pos for node, pos in zip(graph.nodes(), positions_array)}
+
+    edge_width = file_config.line_width
+    for u, v in graph.edges():
+        pos_u = position_dict.get(u)
+        pos_v = position_dict.get(v)
+        if pos_u is not None and pos_v is not None:
+            x_values = [pos_u[0], pos_v[0]]
+            y_values = [pos_u[1], pos_v[1]]
+            ax.plot(x_values, y_values, 'r-', linewidth=edge_width, zorder=2)
+
+    node_size = file_config.node_size
+    for node in graph.nodes():
+        pos = position_dict.get(node)
+        if pos is not None:
+            ax.plot(pos[0], pos[1], 'bo', markersize=node_size, zorder=2)
+
+    if data_type is DataType.ORIGINAL_GRAPH:
+        frame = (0, Config.DEFAULT_FRAME_SIZE[0]), (0, Config.DEFAULT_FRAME_SIZE[1])
+    else:
+        frame = calculate_frame(graph)
+
+    ax.set_xlim(frame[0])
+    ax.set_ylim(frame[1])
+
+    if data_type is DataType.ORIGINAL_GRAPH:
+        image = kwargs['background']
+        alpha = getattr(file_config, 'alpha', 1.0)
+        ax.imshow(image, cmap='gray', extent=(0, image.shape[0], 0, image.shape[1]), alpha=alpha)
+    else:
+        ax.add_patch(
+            plt.Rectangle(
+                (frame[0][0], frame[1][0]),
+                frame[0][1] - frame[0][0],
+                frame[1][1] - frame[1][0],
+                facecolor='none',
+                edgecolor=(0, 0, 0, 0.8),
+                linewidth=2,
+                zorder=1
+            ))
+
+    if 'title' in kwargs:
+        plt.title(kwargs['title'])
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.axis('off')
+    return finalize_plot(fig, getattr(file_config, 'show_on_the_fly', False))
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
 # src/utils/__init__.py
 
-from .utils import build_graph_nodes_and_edges, build_graph_pos_and_adj_mat, calculate_frame, timer, figure_to_ndarray, \
-    plot_network, keep_largest_connected_component
+from .utils import build_graph_nodes_and_edges, build_graph_pos_and_adj_mat, calculate_frame, figure_to_ndarray, \
+    keep_largest_connected_component, timer
