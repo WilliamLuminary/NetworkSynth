c67280554711467e232262aa89f416df58a05f09
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-02-20 00:44:19 -0800

    Sample code instruction

--- src/config/sample/attributes_generate_mode.py
+++ src/config/sample/attributes_generate_mode.py
@@ -15,12 +15,15 @@ class AttributesGenerateModeConfigSample(BaseConfig):
     CLOSED_EDGES_FACTOR = .8
     # For 10_kx image, node fac should be 1.5, and edge fac should be 1
 
-    SYNTHETIC_GRAPH_NUMBER = 0
-    SYNTHETIC_NETWORK_NUMBER = 0
+    SYNTHETIC_GRAPH_NUMBER = 10
+    SYNTHETIC_NETWORK_NUMBER = 300
 
     MAX_ATTEMPTS = 10
 
     BASE_INPUT_PATH = os.path.join(BaseConfig.BASE_INPUT_PATH, 'sample_input', 'generate_mode')
+    ATTRIBUTES_DICT_DATA_PATH = os.path.join(BaseConfig.BASE_INPUT_PATH, 'sample_input', 'property_generate_mode')
+
+    # TODO: The current implementation is would save the data in the same directory as the input data.
 
     @classmethod
     def initialize(cls):
