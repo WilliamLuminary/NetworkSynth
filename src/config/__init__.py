56ef2ef76b82d63fb0c1c0bd2374adcb31c07dba
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-02-20 00:43:56 -0800

    Add AttributesGenerateModeConfigSample import to __init__.py

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,7 +2,7 @@
 
 from .base_config import BaseConfig
 from .config import Config1, Config2, MultifractalConfig
-from .sample import GenerateModeConfigSample
+from .sample import GenerateModeConfigSample, AttributesGenerateModeConfigSample
 
 from .enums import DataType, FileTag, Resolution, SetName, AnalysisMode
 from .file_definitions import FileConfig, ImageConfig, PlotConfig, FILE_CONFIGURATIONS
