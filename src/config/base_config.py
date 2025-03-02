95cb731719e7f5cab58d1cb8d1c819be9d33491d
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-03-01 23:28:07 -0800

    Update TRIM_SIZE default value in base_config.py to (0, 0, 0, 0)

--- src/config/base_config.py
+++ src/config/base_config.py
@@ -21,7 +21,7 @@ class BaseConfig:
     SETS: Union[list, np.ndarray]
     RESOLUTIONS = Union[list, np.ndarray]
 
-    TRIM_SIZE: Tuple[int, int] = None
+    TRIM_SIZE: Tuple[int, int] = (0, 0, 0, 0)
     DEFAULT_FRAME_SIZE: Tuple[int, int] = None
 
     CLOSED_NODES_FACTOR: float
