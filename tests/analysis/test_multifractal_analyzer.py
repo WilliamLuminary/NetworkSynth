70e945a62351ca0bfb070365e60744f3b052e14f
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-03-11 22:23:45 -0700

    Optimize import

--- tests/analysis/test_multifractal_analyzer.py
+++ tests/analysis/test_multifractal_analyzer.py
@@ -6,16 +6,16 @@ import networkx as nx
 import numpy as np
 import pytest
 
-from tests.analysis._original_network_analysis import (
-    nspectrum as original_n_spectrum,
-    wnfd_nk as original_calculate_multifractal_taus,
-    ndimension as original_n_dimension,
-    node_dimension as original_node_dimension,
-    calculate_centralities as original_calculate_centralities,
-    calculate_betweenness as original_calculate_betweenness,
-    calculate_orc as original_calculate_ollivier_ricci_curvature,
-    calculate_assortativity as original_calculate_assortativity,
-    calculate_eigenvector_centrality as original_calculate_eigenvector_centrality,
+from .._original_code import (
+    original_n_spectrum,
+    original_calculate_multifractal_taus,
+    original_n_dimension,
+    original_node_dimension,
+    original_calculate_centralities,
+    original_calculate_betweenness,
+    original_calculate_ollivier_ricci_curvature,
+    original_calculate_assortativity,
+    original_calculate_eigenvector_centrality
 )
 from analysis.multifractal_analyzer import MultifractalAnalyzer
 from config import BaseConfig, AnaConfig
