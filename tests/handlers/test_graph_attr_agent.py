0ca48d7a55ec96c4f7c08a49779262328cda0ec5
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-03-15 23:21:56 -0700

    Move

--- tests/handlers/test_graph_attr_agent.py
+++ tests/handlers/test_graph_attr_agent.py
@@ -5,17 +5,3 @@ from typing import Dict
 from handlers import AttributesCalculator
 
 
-
-
-def test_pickle_dump_graph_attr_agent(load_graph_from_pickle):
-    sample_graph = load_graph_from_pickle
-    agent = AttributesCalculator()
-    agent.analyze(sample_graph)
-    with open("attr_dict.pkl", "wb") as f:
-        # noinspection PyTypeChecker
-        pickle.dump(agent, f)
-
-    assert os.path.exists("attr_dict.pkl")
-    with open("attr_dict.pkl", "rb") as f:
-        load_file = pickle.load(f)
-    assert isinstance(load_file, Dict)
