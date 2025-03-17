62cb23ae6e9bec69c0091eb975e67367a921a306
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-03-16 19:18:42 -0700

    Pytest: Remove tester code for new AttributesCalculator since upgraded

--- tests/handlers/test_attributes_calculator.py
+++ tests/handlers/test_attributes_calculator.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 import pytest
 
-from handlers.attributes_calculator import AttributesCalculator, NewAttributesCalculator
+from handlers.attributes_calculator import AttributesCalculator
 
 
 def compare_dicts_of_floats(dict_a: dict[int, float], dict_b: dict[int, float], abs_tol: float = 1e-7):
@@ -37,14 +37,13 @@ def compare_dicts_of_lists_of_floats(dict_a: dict[int, list[float]], dict_b: dic
 
 def test_attributes_calculators_equivalence(load_weighted_test_nx_graph):
     graph = load_weighted_test_nx_graph
-    old_calc = AttributesCalculator().analyze(graph)
-    new_calc = NewAttributesCalculator().analyze(graph)
-    assert old_calc.average_degree == pytest.approx(new_calc.average_degree, abs=1e-7)
-    assert old_calc.average_edge_length == pytest.approx(new_calc.average_length, abs=1e-7)
-    compare_dicts_of_floats(old_calc.degree_distribution, new_calc.degree_distribution)
-    compare_dicts_of_dicts_of_floats(old_calc.degree_transition_probs, new_calc.degree_transition_probs)
-    compare_dicts_of_lists_of_floats(old_calc.degree_edge_lengths, new_calc.degree_lengths)
-    compare_dicts_of_lists_of_floats(old_calc.degree_angle_diffs, new_calc.degree_angles)
+    _ = AttributesCalculator().analyze(graph)
+    # assert old_calc.average_degree == pytest.approx(new_calc.average_degree, abs=1e-7)
+    # assert old_calc.average_length == pytest.approx(new_calc.average_length, abs=1e-7)
+    # compare_dicts_of_floats(old_calc.degree_distribution, new_calc.degree_distribution)
+    # compare_dicts_of_dicts_of_floats(old_calc.degree_transition_probs, new_calc.degree_transition_probs)
+    # compare_dicts_of_lists_of_floats(old_calc.degree_lengths, new_calc.degree_lengths)
+    # compare_dicts_of_lists_of_floats(old_calc.degree_angles, new_calc.degree_angles)
     print("All calculator attributes matched (within numerical tolerance) between old and new implementations.")
 
 
@@ -52,7 +51,7 @@ def test_pickle_io(load_unweighted_test_nx_graph):
     sample_graph = load_unweighted_test_nx_graph
     attr_cal = AttributesCalculator()
     attr_cal.analyze(sample_graph)
-    pickle_file_path = os.path.join("..", "data", "attr_dict.pkl")
+    pickle_file_path = os.path.join("data", "attr_dict.pkl")
     _test_dump(attr_cal, pickle_file_path)
     loaded_data = _test_dump_type(pickle_file_path)
     _test_reconstruction(attr_cal, loaded_data)
