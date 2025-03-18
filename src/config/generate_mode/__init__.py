eee92d46b8fffa634f80b1fafaed24139a13104c
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-03-17 21:27:39 -0700

    Add igraph sample code

--- src/config/generate_mode/__init__.py
+++ src/config/generate_mode/__init__.py
@@ -4,4 +4,5 @@
 # Others are named as ".*Config\d+"
 from .config_1 import Config1 as GenConfig1
 from .config_2 import Config2 as GenConfig2
-from .sample import SampleConfig as GenConfig
+from .sample_pos_and_mat import SampleConfig as GenConfig
+from .sample_igraph import SampleConfig as IgraphGenConfig
