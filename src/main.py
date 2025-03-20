ec13e83bd6c9b8394ef06fb459cb56bb5a5f120a
William <93109493+WilliamLuminary@users.noreply.github.com>
2025-03-19 20:21:33 -0700

    Rename

--- src/main.py
+++ src/main.py
@@ -8,12 +8,12 @@ import numpy as np
 from analysis import MultifractalAnalyzer
 from config import BaseConfig, DataType, Resolution, SetName
 # noinspection PyUnresolvedReferences
-from config import GenConfig, GenConfig1, GenConfig2, IgraphGenConfig
+from config import GenConfig, GenConfig1, GenConfig2, LinGenConfig
 from graph import GraphGenerator
 from handlers import AttributesCalculator, Mapper, RunAgent, Saver
 from utils import trim_graph
 
-IgraphGenConfig.initialize()
+LinGenConfig.initialize()
 # BaseConfig.disable_saving("Preview")
 
 logger = logging.getLogger(__name__)
