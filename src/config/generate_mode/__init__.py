35a4862b76b98b440d53e390f35142555361406d
Yaxing (William) <93109493+WilliamLuminary@users.noreply.github.com>
2026-01-15 20:03:43 -0800

    Reformat (#10)
    Add configs for nanowires.py

--- src/config/generate_mode/__init__.py
+++ src/config/generate_mode/__init__.py
@@ -4,4 +4,5 @@
 # Others are named as ".*Config\d+"
 from .config_1 import Config1 as GenConfig1
 from .config_2 import Config2 as GenConfig2
+from .config_nanowires import NanowiresConfig as GenConfigNanowires
 from .config_sample import SampleConfig as GenConfig
